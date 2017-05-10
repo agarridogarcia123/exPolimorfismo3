51601c5b6968a9624d78c3b0312bc56c9ba75082
agarridogarcia <agarridogarcia@nautilus5.danielcastelao.org>
2017-05-10 09:40:55 +0200

    exemplo polimorfismo 3

---
+++ src/expolimorfismo/Icalculable.java
@@ -0,0 +1,11 @@
+
+package expolimorfismo;
+//interfaz funcional solo ten metodos abstractos,a maiores pode ter metodos static ou default
+@FunctionalInterface
+
+public interface Icalculable {
+    
+    
+    public abstract float calcularPrezo();
+    
+}
